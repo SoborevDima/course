20a48d7266f275c237120681ad926d9048a9ab01
Soborev_Dima <SoborevDima2001@gmail.com>
2025-11-25 21:30:45 +0300

    2 варианта циклов js, поток js и условия if

---
+++ js-start/main.js
@@ -0,0 +1,41 @@
+console.log("Hello from js");
+
+// alert ('Hi Dima');
+
+// document.write("<h1>Hello from JS</h1>");
+
+// const age = prompt("Сколько вам лет?");
+
+// let access = "";
+
+// if (age > 18) {
+//     access = "Доступ разрешен";
+// } else if (age < 18 && age >=16 && age != 0) {
+//     access = "Доступ запрещен. Приходи через пару лет";
+// } else {
+//     access = "Доступ запрещен";
+// }
+
+// document.write(`<h4>${access}</h4>`);
+
+
+//let access = age > 18 ? "Доступ разрешен" : "Доступ запрещен";
+
+
+//Циклы
+
+
+// let num = 0;
+// let age = prompt("Сколько вам лет?");
+
+// while (true) {
+//     if (age > 0 && age < 100) {
+//         break;
+//     }
+
+//     age = prompt ("Некорректный ввод. Сколько вам");
+// }
+
+for (let i = 0; i < 10; i++) {
+    document.write(`<h4>${i}</h4>`);
+}
