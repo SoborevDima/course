97f5c7166d7c86c72055393b77a939297e68c5f1
Soborev_Dima <SoborevDima2001@gmail.com>
2025-12-08 09:24:25 +0300

    дописал function nextSlide, prevSlide для кнопок

--- JS - slider/main.js
+++ JS - slider/main.js
@@ -4,9 +4,34 @@ const img = document.querySelector
 const imgArr = ['/JS - slider/img/1.jpg, /JS - slider/img/2.jpg, /JS - slider/img/3.jpg']
 
 let currentIndex = 0;
+const imgElem: HTMLElement = document.getElementByID(elementid"img");
+imgElem.src = imgArr[currentIndex]
+
+function nextSlide():void {
+    if (currentIndex < imgArr.length - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+
+
+    imgElem.src = imgArr[currentIndex]
+}
+
+function prevSlide():void {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = imgArr.length - 1;
+    }
+
+
+    imgElem.src = imgArr[currentIndex]
+}
+
 
 function slide(document) {
     currentIndex += direction;
 
-    img.scr = imgArr[currentIndex];
+    img.src = imgArr[currentIndex];
 }
