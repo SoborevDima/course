ded3d1d982ac7c8b288dbd196b0079a363c8bd3a
Soborev_Dima <SoborevDima2001@gmail.com>
2025-12-20 22:46:56 +0300

    сделал активные кнопки по слайдам

--- JS - slider/main.js
+++ JS - slider/main.js
@@ -1,17 +1,29 @@
 const img = document.querySelector 
 ('.slider__image');
 
+const dot = document.querySelectorAll
+(".slider__dot");
+
 const imgArr = ["/JS - slider/img/1.jpg", "/JS - slider/img/2.jpg","/JS - slider/img/3.jpg"];
 
 let currentIndex = 0;
 
-function slide(direction) {
-    currentIndex += direction;
+function changeIndex(ind) {
+    currentIndex = ind;
+    slide(currentIndex);
+}
+
+function nextIndex(direction) {
+        currentIndex += direction;
     if (currentIndex >= imgArr.length) {
         currentIndex = 0;
     }   else if (currentIndex < 0) {
         currentIndex = imgArr.length - 1;
     }
 
-    img.src = imgArr[currentIndex];
+    slide(currentIndex);
+}
+
+function slide(index) {
+    img.src = imgArr[index];
 }
