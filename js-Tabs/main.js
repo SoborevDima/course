ac957ab6244ff999d564edec1bdf57124d6f47cd
Soborev_Dima <SoborevDima2001@gmail.com>
2026-01-23 15:58:26 +0300

    Создание табов (класс активный не совпадает с контентом, разобраться)

---
+++ js-Tabs/main.js
@@ -0,0 +1,20 @@
+const tabs = document.querySelectorAll(".tab-head__item");
+const contents = document.querySelectorAll(".tab-content__item");
+
+tabs.forEach((tab, index) => {
+    tab.onclick = () => {
+        changeTab(index);
+    };
+});
+
+function changeTab(index) {
+    for (let tab of tabs) {
+        tab.classList.remove("active");
+    }
+    tabs[index].classList.add("active");
+
+    for(let el of contents) {
+        el.classList.remove("active");
+    }
+    contents[index].classList.add("active");
+}
